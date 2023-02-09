22cab6d90b7ab1be770e1ebfb15a1bde8ceed9a8
Tamim Arefin Anik <42251521+tamim36@users.noreply.github.com>
2023-02-09 15:23:13 +0600

    Time: 113 ms (88.36%), Space: 59.7 MB (23.71%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -5,9 +5,7 @@ public:
 
     while (left < right) {
         int mid = left + (right - left) / 2;
-        if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1])
-            return mid;
-        else if (arr[mid - 1] > arr[mid])
+        if (arr[mid] >= arr[mid+1])
             right = mid;
         else
             left = mid + 1;
