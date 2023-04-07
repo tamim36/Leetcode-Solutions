7134ac91a96db964d5aef5603b9ba3914d505d93
Tamim Arefin Anik <42251521+tamim36@users.noreply.github.com>
2023-04-07 10:05:32 +0600

    Time: 23 ms (37.90%), Space: 16 MB (24.72%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,24 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        vector<vector<int>> ans;
-    queue<vector<int>> q;
-    int des = graph.size()-1, src = 0;
-    q.push({0});
-
-    while (!q.empty()) {
-        auto top = q.front(); q.pop();
-        int curDes = top[top.size() - 1];
-        if (curDes == des) {
-            ans.push_back(top);
-            continue;
+        // edges cases:
+        if (graph.empty()) {
+            return {};
         }
-        for (auto edge : graph[curDes]) {
-            top.push_back(edge);
-            q.push(top);
-            top.pop_back();
+        
+        // build di-graph
+        unordered_map<int, vector<int>> d;
+        for (int i = 0; i < graph.size(); i++) {
+            d[i] = graph[i]; // one-way link
         }
-    }
-    return ans;
+        
+        // apply DFS on DAG
+        int n = graph.size();
+        vector<pair<int, vector<int>>> stack = {{0, {0}}}; // - store both the (node, and the path leading to it)
+        vector<vector<int>> res;
+        while (!stack.empty()) {
+            int node = stack.back().first;
+            vector<int> path = stack.back().second;
+            stack.pop_back();
+            // check leaf
+            if (node == n - 1) {
+                res.push_back(path);
+            }
+            // traverse rest
+            for (int nei : d[node]) {
+                path.push_back(nei);
+                stack.push_back({nei, path});
+                path.pop_back();
+            }
+        }
+        return res;
     }
 };
