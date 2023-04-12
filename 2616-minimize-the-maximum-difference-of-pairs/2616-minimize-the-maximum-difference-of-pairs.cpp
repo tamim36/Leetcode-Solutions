f6b1fa5236da39c42c2b86163e6b633e7f8026ca
Tamim Arefin Anik <42251521+tamim36@users.noreply.github.com>
2023-04-12 11:25:29 +0600

    Time: 187 ms (65.21%), Space: 81.3 MB (37.22%) - LeetHub

---
+++ 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isValidMinimizeMax(vector<int>& nums, int p, int x) {
+    for (int i = 0; p > 0 && i < nums.size()-1; i++) {
+        if ((nums[i + 1] - nums[i]) <= x) {
+            
+            p--;
+            i++;
+        }
+    }
+    if (p <= 0)
+        return true;
+    else
+        return false;
+}
+
+int minimizeMax(vector<int>& nums, int p) {
+    if (!p || nums.empty())
+        return 0;
+
+    sort(nums.begin(), nums.end());
+
+    int sz = nums.size(), left = 0, right = (nums[sz-1] - nums[0] + 1);
+
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+        if (isValidMinimizeMax(nums, p, mid))
+            right = mid;
+        else
+            left = mid + 1;
+    }
+
+    return right;
+}
+};
