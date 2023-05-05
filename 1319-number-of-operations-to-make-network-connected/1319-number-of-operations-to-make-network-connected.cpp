dbd79b2bba6234feb922232c874854a9acbc976e
Tamim Arefin Anik <42251521+tamim36@users.noreply.github.com>
2023-05-06 01:14:19 +0600

    Time: 199 ms (8.41%), Space: 37.9 MB (90.97%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,34 +1,48 @@
 class Solution {
 public:
-    int makeConnected_helperDfs(vector<vector<int>>& graph, vector<int>& vis, int node) {
-    if (vis[node]) return 0;
+    int makeConnected_find(vector<int>& parent, int u) {
+    if (parent[u] == u)
+        return u;
 
-    vis[node] = 1;
-    for (int i = 0; i < graph[node].size(); i++) {
-        makeConnected_helperDfs(graph, vis, graph[node][i]);
+    return parent[u] = makeConnected_find(parent, parent[u]);
+}
+
+int makeConnected_union(vector<int>& parent, vector<int>& rank, int u, int v) {
+    u = makeConnected_find(parent, u);
+    v = makeConnected_find(parent, v);
+
+    if (u == v) return 0;
+    cout << rank[u] << "  " << rank[v] << endl;
+    if (rank[u] > rank[v]) {
+        parent[v] = u;
+        rank[u]++;
     }
-
+    else {
+        parent[u] = v;
+        rank[v]++;
+    }
+    
     return 1;
 }
 
+// 1319
 int makeConnected(int n, vector<vector<int>>& connections) {
     if (connections.size() < n - 1) return -1;
-    vector<int> vis(n, 0);
-    vector<vector<int>> graph(n);
+    vector<int> parent(n, 0), rank(n, 0);
     int component = 0;
+
+    for (int i = 0; i < n; i++) {
+        rank[i] = 1;
+        parent[i] = i;
+    }
 
     for (auto& edge : connections) {
         int u = edge[0];
         int v = edge[1];
 
-        graph[u].push_back(v);
-        graph[v].push_back(u);
+        component += makeConnected_union(parent, rank, u, v);
     }
 
-    for (int i = 0; i < n; i++) {
-        component += makeConnected_helperDfs(graph, vis, i);
-    }
-
-    return component - 1;
+    return n - component - 1;
 }
 };
