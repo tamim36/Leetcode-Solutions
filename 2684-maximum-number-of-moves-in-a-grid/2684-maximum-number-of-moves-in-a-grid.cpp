bc94ca761a7d25acc1bdf2c7d807f938acbb0848
Tamim Arefin Anik <42251521+tamim36@users.noreply.github.com>
2023-05-15 09:35:27 +0600

    Time: 236 ms (69.23%), Space: 62.4 MB (84.62%) - LeetHub

--- 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
@@ -1,48 +1,26 @@
 class Solution {
 public:
-    int maxMoves(vector<vector<int>>& grid) {
-        //cout << grid[0][61] << endl;
-        int dx[] = { -1, 0, 1 };
-        int dy[] = { 1, 1, 1 };
-        int m = grid.size(), n = grid[0].size();
-    int mx = 0, lvl = 0;;
+    int mvMov = 0;
+void dfsMaxMoves(vector<vector<int>>& grid, int m, int n, int r, int c, int prev, int mov) {
+    if (r < 0 || c < 0 || r >= m || c >= n || !grid[r][c] || grid[r][c] <= prev) return;
+    //cout << prev << "  ";
+    prev = grid[r][c];
+    grid[r][c] = 0;
 
-    queue<pair<int, int>> q;
+    int dx[] = {-1, 0, 1};
+    int dy[] = {1, 1, 1};
+    ++mov;
+    mvMov = max(mov, mvMov);
+    for (int i = 0; i < 3; i++)
+        dfsMaxMoves(grid, m, n, r+dx[i], c+dy[i], prev, mov);
+}
+
+int maxMoves(vector<vector<int>>& grid) {
+    int m = grid.size(), n = grid[0].size();
 
     for (int i = 0; i < m; i++) {
-        q.push({i, 0});
-
-        while (!q.empty())
-        {
-            int sz = q.size();
-            lvl++;
-            for (int j = 0; j < sz; j++) {
-                auto top = q.front(); q.pop();
-                int a = top.first, b = top.second;
-                //cout << a << "  " << b << endl;
-                if (!grid[a][b])
-                    continue;
-                int prev = grid[a][b];
-                grid[a][b] = 0;
-
-                
-
-                for (int i = 0; i < 3; i++) {
-                    int r = a + dx[i], c = b + dy[i];
-                    
-                    
-                    if (r >= 0 && c >= 0 && r < m && c < n && grid[r][c] && grid[r][c] > prev) {
-                        //cout << "r " << r << "  c  " << c << " m " << m << " n " << n << endl;
-                        q.push({ r, c });
-                    }
-                }
-            }
-        }
-
-        mx = max(lvl-1, mx);
-        if (mx == n - 1) return mx;
-        lvl = 0;
+        dfsMaxMoves(grid, m, n, i, 0, 0, 0);
     }
-    return mx;
-    }
+    return mvMov-1;
+}
 };
