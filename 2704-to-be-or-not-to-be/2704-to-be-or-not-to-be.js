2e0e93c9e3b20b8b833de903a3497627f58bd083
Tamim Arefin Anik <42251521+tamim36@users.noreply.github.com>
2024-02-13 11:29:58 +0600

    Time: 57 ms (32.05%), Space: 48.3 MB (35.76%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe : (anotherVal) => {
+            if (val === anotherVal){
+                return true
+            }
+            throw new Error('Not Equal')
+        },
+    
+        notToBe : (anotherVal) => {
+            if (val !== anotherVal){
+                return true
+            }
+            throw new Error('Equal')
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
