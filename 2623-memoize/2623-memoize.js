dedbcfc4ef06214b97764569f6c05267e3a3e34d
Tamim Arefin Anik <42251521+tamim36@users.noreply.github.com>
2024-04-25 01:54:15 +0600

    Time: 304 ms (54.43%), Space: 90.3 MB (25.84%) - LeetHub

--- 2623-memoize/2623-memoize.js
+++ 2623-memoize/2623-memoize.js
@@ -3,16 +3,24 @@
  * @return {Function}
  */
 function memoize(fn) {
-    let cache = []
-    return function(...args) {
-        if (cache[args] != undefined)
-            return cache[args];
-        
-        let val = fn(...args);
-        cache[args] = val;
-        return cache[args]
+    
+   const cache = {};
+  
+   return function(...args) {
+    const key = JSON.stringify(args);
+    
+    if (key in cache) {
+      return cache[key];
     }
+    
+    const result = fn.apply(this, args);
+    cache[key] = result;
+    
+    return result;
+  }
+  
 }
+
 
 
     /** 
