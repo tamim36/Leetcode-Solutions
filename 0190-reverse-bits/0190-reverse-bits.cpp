58a316ba7deb2b628f0ef16a0b59d24af7ef173a
Tamim Arefin Anik <42251521+tamim36@users.noreply.github.com>
2024-05-24 12:15:05 +0600

    Time: 4 ms (36.72%), Space: 6.9 MB (92.41%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -2,18 +2,14 @@ class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
         if (n == 0)
+            return n;
+
+    n = ((n & 0xffff0000) >> 16) | ((n & 0x0000ffff) << 16);
+    n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8);
+    n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4);
+    n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2);
+    n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1);
+
     return n;
-       uint32_t rev = 0;
-int i = 0;
-
-while (n) {
-    rev <<= 1;
-    if (n & 1) 
-        rev |= 1;
-    i++;
-    n >>= 1;
-}
-
-return rev << (32 - i);
     }
 };
