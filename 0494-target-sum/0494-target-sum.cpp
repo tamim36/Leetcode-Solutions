0cf56a0a7c978e90c15c0827c9c43dd90206a305
Tamim Arefin Anik <42251521+tamim36@users.noreply.github.com>
2024-10-13 00:43:13 +0600

    Time: 3 ms (92.40%), Space: 14.9 MB (37.21%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-   unordered_map<int, unordered_map<int, int>> memo;
-
-    int findTargetSumWays(vector<int>& nums, int target, int curSum, int n) {
-        // Base case: when n is 0, check if the current sum is equal to the target
-        if (n == 0) 
-            return curSum == target ? 1 : 0;
-        
-        // Check if the result for the current subproblem (n, curSum) is already memoized
-        if (memo[n].find(curSum) != memo[n].end())
-            return memo[n][curSum];
-
-        // Recursively calculate the number of ways by adding and subtracting the current element
-        int add = findTargetSumWays(nums, target, curSum + nums[n - 1], n - 1);
-        int subtract = findTargetSumWays(nums, target, curSum - nums[n - 1], n - 1);
-
-        // Store the result in the memo table before returning
-        memo[n][curSum] = add + subtract;
-
-        return memo[n][curSum];
-    }
-
     int findTargetSumWays(vector<int>& nums, int target) {
-        return findTargetSumWays(nums, target, 0, nums.size());
+        if (target < 0) target *= (-1);
+int n = nums.size();
+int sum = accumulate(nums.begin(), nums.end(), 0);
+
+if ((target + sum) % 2)
+    return 0;
+
+int t = (target + sum) / 2;
+vector<vector<int>> dp(n + 1, vector<int>(t + 1, 0));
+
+for (int i = 0; i < n; i++)
+    dp[i][0] = 1;
+
+for (int i = 1; i <= n; i++) {
+    for (int j = 0; j <= t; j++) {
+        if (j - nums[i - 1] >= 0)
+            dp[i][j] = dp[i - 1][j] + dp[i - 1][j - nums[i - 1]];
+        else
+            dp[i][j] = dp[i - 1][j];
+    }
+}
+
+return dp[n][t];
     }
 };
