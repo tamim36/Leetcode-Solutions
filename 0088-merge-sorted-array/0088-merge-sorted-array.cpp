3341f62e2c2dce51461002d540267db89ed1a339
Tamim Arefin Anik <42251521+tamim36@users.noreply.github.com>
2026-09-19 11:18:06 +0600

    Time: 0 ms (100%), Space: 12.3 MB (86.71%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int idx = m + n - 1;
-        int i = m - 1, j = n - 1;
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    int mIdx = m - 1, nIdx = n - 1;
 
-        for (; i >= 0 && j >= 0; idx--) {
-            if (nums1[i] > nums2[j])
-                nums1[idx] = nums1[i--];
-            else
-                nums1[idx] = nums2[j--];
+    for (int i = m + n - 1; i >= 0; i--){
+        if (mIdx >= 0 && nIdx >= 0){
+            if (nums1[mIdx] > nums2[nIdx]) nums1[i] = nums1[mIdx--];
+            else nums1[i] = nums2[nIdx--]; 
         }
-
-        while (i >= 0)
-            nums1[idx--] = nums1[i--];
-
-        while (j >= 0)
-            nums1[idx--] = nums2[j--];
+        else if (mIdx >= 0) nums1[i] = nums1[mIdx--];
+        else nums1[i] = nums2[nIdx--]; 
     }
+}
 };
